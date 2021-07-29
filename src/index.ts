365603c46f6652f822a384b3ece61b0ead7420d0
AndhyGT <andhyralac11@gmail.com>
2021-07-29 06:44:27 -0600

    handlebars configuration for the project

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
+import path from 'path';
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    helpers: require('./lib/helpers')
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.json());
